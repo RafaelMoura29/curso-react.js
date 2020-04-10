300e1d46fb695b74177454b24c18c4b3d799f812
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-04-10 11:40:07 -0300

    Aprendendo statefull components

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,45 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-            <Social fb={props.facebook}/>
-            <hr></hr>
-        </div>
-    )
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: 'Matheus',
+            contador: 0
+        }
+        this.aumentar = this.aumentar.bind(this)
+        this.diminuir = this.diminuir.bind(this)
+    }
+
+    aumentar(){
+        let state = this.state
+        state.contador +=1;
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state
+        if(state.contador !== 0)
+            state.contador -=1;
+        else    
+            alert("chegou a zero")
+        this.setState(state)
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>
+                    <button onClick={this.diminuir}>-</button>
+                    {this.state.contador}
+                    <button onClick={this.aumentar}>+</button>
+                </h3>
+            </div>
+        )
+    }
 }
-
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá sou o {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-        </div>
-    );
-}
-
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.fb}>Facebook: </a>
-            <a>Linkedin: </a>
-            <a>Youtube: </a>
-        </div>
-    );
-}
-
-function App() {
-    return (
-        <div>
-            <h1>Conheça nossa equipe</h1>
-            <Equipe nome="Lucas" cargo="Programador" idade="29" 
-                    facebook="facebook.com"/>
-            <Equipe nome="Pedro" cargo="Designer" idade="35" 
-                    facebook="facebook.com"/>
-        </div>
-    )
-}
-
 export default App;
