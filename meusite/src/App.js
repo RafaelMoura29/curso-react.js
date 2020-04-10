797d2ece47392c5a3b0e81c131ff3216dc4cacf4
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-04-10 20:21:47 -0300

    Utilizando o ciclo de vida do component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -2,39 +2,27 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
-            nome: 'Matheus',
-            contador: 0
+            hora: "00:00"
         }
-        this.aumentar = this.aumentar.bind(this)
-        this.diminuir = this.diminuir.bind(this)
     }
 
-    aumentar(){
-        let state = this.state
-        state.contador +=1;
-        this.setState(state)
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({hora: new Date().toLocaleTimeString()})
+        }, 1000)
     }
 
-    diminuir(){
-        let state = this.state
-        if(state.contador !== 0)
-            state.contador -=1;
-        else    
-            alert("chegou a zero")
-        this.setState(state)
+    componentDidUpdate(){
+        console.log("Atualizou")
     }
 
     render() {
         return (
             <div>
-                <h3>
-                    <button onClick={this.diminuir}>-</button>
-                    {this.state.contador}
-                    <button onClick={this.aumentar}>+</button>
-                </h3>
+                <h1>Meu projeto {this.state.hora}</h1>
             </div>
         )
     }
