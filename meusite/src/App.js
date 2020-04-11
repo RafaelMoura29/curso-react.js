5fac31ba5a802e2339920c02d9be956e74a32ed7
Rafael Moura <rafaelmoura2929@gmail.com>
2020-04-11 15:26:05 -0300

    Requests http

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,57 +1,44 @@
 import React, { Component } from 'react';
-import './estilos.css'
-
+import './style.css'
+//https://sujeitoprogramador.com/rn-api/?api=posts
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            numero: 0,
-            botao: "iniciar"
-        }
-        this.timer = null
-        this.limpar = this.limpar.bind(this)
-        this.vai = this.vai.bind(this)
-    }
-
-    vai() {
-
-        if (this.timer !== null) {
-            clearInterval(this.timer)
-            this.timer = null
-            let state = this.state
-            state.botao = "iniciar"
-            this.setState(state)
-        } else {
-            this.timer = setInterval(() => {
-                let state = this.state
-                state.numero += 0.1
-                state.botao = "pausar"
-                this.setState(state)
-            }, 100)
+            nutri: []
         }
     }
 
-    limpar() {
-        if (this.timer !== null) {
-clearInterval(this.timer)            
-this.timer = null
-        }
-        let state = this.state
-        state.botao = "iniciar"
-        state.numero = 0
-        this.setState(state)
+    componentDidMount() {
+        let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+        fetch(url)
+            .then(r => {
+                r.json()
+                    .then(json => {
+                        let state = this.state
+                        state.nutri = json
+                        this.setState(state)
+                    })
+            })
     }
 
     render() {
         return (
             <div className="container">
-                <img alt="cronometro" src={require('./assets/cronometro.jpg')} className="img" />
-                <p className="timer" href="#">{this.state.numero.toFixed(1)}</p>
-                <div className="areaBtn">
-                    <a className="botao" onClick={this.vai}>{this.state.botao}</a>
-                    <a className="botao" onClick={this.limpar}>Limpar</a>
-                </div>
+                <header>
+                    <strong>React Nutri</strong>
+                </header>
+                {this.state.nutri.map(item => {
+                    return (
+                        <article Key={item.id} className="post">
+                            <strong className="titulo">{item.titulo}</strong>
+                            <img src={item.capa} className="capa"/>
+                            <p className="subtitulo">{item.subtitulo}</p>
+                            <a href="#"className="botao">Acessar</a>
+                        </article>
+                    )
+                })}
             </div >
         )
     }
